b9f671263dad75063d00dc4304ace2a986dd3beb
Nathan Faucett <nathanfaucett@gmail.com>
2015-03-14 21:04:55 -0500

    updates

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ module.exports = Contact2;
 
 
 function Contact2() {
-    this.normal = vec2.create();
-    this.point = vec2.create();
+    this.normal = vec2.create(0, 1);
+    this.point = vec2.create(0, 0);
     this.depth = 0;
 }
 createPool(Contact2);
@@ -18,4 +18,10 @@ Contact2.create = function() {
 
 Contact2.prototype.destroy = function() {
     return Contact2.release(this);
+};
+
+Contact2.prototype.destructor = function() {
+    vec2.set(this.normal, 0, 1);
+    vec2.set(this.point, 0, 0);
+    this.depth = 0;
 };
